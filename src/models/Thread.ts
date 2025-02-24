0ce5a6ec6a7d5b854a30308fd0eb9c091fe983a2
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-02-24 18:26:51 +0700

    Update Thread.ts

--- src/models/Thread.ts
+++ src/models/Thread.ts
@@ -3,11 +3,14 @@ import mongoose from "mongoose";
 
 const ThreadSchema = new mongoose.Schema(
   {
-    id_thread: { type: String, required: true, unique: true },
+    _id: { type: String, required: true, unique: true },
     title: { type: String, required: true },
     user_id: { type: String, ref: "User", required: true }, 
   },
   { timestamps: true }
 );
 
+// ThreadSchema.virtual("id_thread").get(function () {
+//   return this._id;
+// });
 export default mongoose.model("Thread", ThreadSchema);
