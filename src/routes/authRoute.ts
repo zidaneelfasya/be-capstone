d39b83e88ce705449f7cd800043df0e9d79a145d
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-03-02 12:22:28 +0700

    Update authRoute.ts

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,11 +1,14 @@
 import express from "express";
 
-import { login, logout, register } from "../controllers/authController";
+import { checkAuth, login, logout, register } from "../controllers/authController";
 
 const router = express.Router();
 
 router.post("/register", register);
+
 router.post("/login", login);
+router.get("/check", checkAuth);
+
 router.post("/logout", logout);
 
 export default router;
