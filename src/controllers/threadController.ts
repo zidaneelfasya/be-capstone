b7a3480d467b1adf7b61894518f1a9667bb950e3
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-03-02 12:22:20 +0700

    Update threadController.ts

--- src/controllers/threadController.ts
+++ src/controllers/threadController.ts
@@ -6,18 +6,21 @@ export const createThread = async (req: any, res: any) => {
   try {
     await verifyAPI(req, res);
     await connectToDatabase();
-    const me = req.user.payload.id;
+    const me = req.user.id;
 
     const { title } = req.body;
     const newId = crypto.randomUUID();
     const newThread = new Thread({ _id: newId, title, user_id: me });
-    console.log(newId)
+    console.log(newId);
     const savedThread = await newThread.save();
-    
 
     return res
       .status(201)
-      .json({ message: "User registered successfully", thread: savedThread });
+      .json({
+        message: "User registered successfully",
+        thread: savedThread,
+        user: me,
+      });
   } catch (error: any) {
     return res
       .status(500)
@@ -40,18 +43,17 @@ export const getAllThreads = async (req: any, res: any) => {
 };
 
 export const getThreads = async (req: any, res: any) => {
-  await verifyAPI(req, res);
-  await connectToDatabase();
-  const me = req.user.payload.id;
   try {
-    const threads = await Thread.find({
-      user_id: me,
-    }).populate("user_id", "username _id").sort({ updatedAt: -1 });
-   
-    return res.status(200).json({
-      message: "Success",
-      data: threads,
-    });
+    await verifyAPI(req, res);
+    await connectToDatabase();
+
+    const me = req.user.id; 
+
+    const threads = await Thread.find({ user_id: me })
+      .populate("user_id", "username _id")
+      .sort({ updatedAt: -1 });
+
+    return res.status(200).json({ message: "Success", data: threads });
   } catch (error: any) {
     return res
       .status(500)
