dce6aaae9cfe08b8fbf645664f76ac90f8917fdf
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-03-03 23:16:55 +0700

    Update threadRoute.ts

--- src/routes/threadRoute.ts
+++ src/routes/threadRoute.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { createThread, getThreads } from "../controllers/threadController";
+import { createThread, getThreads, getThreadsUser } from "../controllers/threadController";
 
 const router = express.Router();
 
 router.post("/add", createThread);
 
-router.get("/", getThreads)
+// router.get("/", getThreads)
+router.get("/", getThreadsUser)
+
 
 export default router;
